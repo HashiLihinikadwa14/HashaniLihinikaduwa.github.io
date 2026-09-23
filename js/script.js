383a7de5e56873d1133d5486214fdb77776820e5
Hashani Lihinikaduwa <hashilihinikaduwa456@gmail.com>
2026-09-23 11:21:28 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Welcome message
+console.log("Welcome to Hashi's Portfolio!");
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
